2c413e31de902c5c0b9252d551e6a7d874e1ae00
r.makretski <r.makretski@andersenlab.com>
2021-09-22 00:55:01 +0300

    add: added import service integration

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://j90gusfcc8.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://wz6evpf3hd.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
